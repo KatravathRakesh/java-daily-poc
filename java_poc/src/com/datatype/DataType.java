003b5fe1f62e299030458e7729d6ba93459b4199
KATRAVATH RAKESH <rakeyrakesh2233@gmail.com>
2026-07-06 21:25:58 +0530

    Day-07 Data Type

---
+++ java_poc/src/com/datatype/DataType.java
@@ -0,0 +1,30 @@
+package com.datatype;
+
+public class DataType {
+
+	byte b ;
+	short s;
+	int i;
+	long l;
+	float f;
+	double d;
+	char c;
+	boolean bo;
+	
+	
+	public static void main(String[] args) {
+		DataType s = new DataType();
+		System.out.println("Byte b : "+s.b);
+		System.out.println("short s : "+s.s);
+		System.out.println("int i : "+s.i);
+		System.out.println("long l : "+s.l);
+		System.out.println("float f : "+s.f);
+		System.out.println("double d : "+s.d);
+		System.out.println("char c : "+s.c);
+		System.out.println("boolean bo : "+s.bo); 
+		
+		
+		
+	}
+
+}
