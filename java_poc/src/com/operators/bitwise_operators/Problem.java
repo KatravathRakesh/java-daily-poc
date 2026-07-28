15415460b39eeb2c65b42503a80aa1be478b67e6
KATRAVATH RAKESH <rakeyrakesh2233@gmail.com>
2026-07-28 15:05:02 +0530

    Day-24 BitWise Operators POCs

---
+++ java_poc/src/com/operators/bitwise_operators/Problem.java
@@ -0,0 +1,36 @@
+package com.operators.bitwise_operators;
+
+
+//Bitwise Operators (&, |, ^, ~)
+
+public class Problem {
+
+	public static void main(String[] args) {
+//		Find 5 & 3.
+//		5 --> 101
+//		3 --> 011
+		System.out.println(5 & 3);//1
+//		Find 5 | 3.
+		System.out.println(5 | 3);//7
+//		Find 5 ^ 3.
+		System.out.println(5 ^ 3);//6
+//		Find ~5.
+//		~5 --> -(n+1) --> -(5+1) = -6
+		System.out.println(~5);//-6
+//		Find ~10.
+		System.out.println(~10);//-11
+		
+//		Compare & and &&.
+//		& -> don't have dead code and it use number for compare
+//		&& -> have dead code and it not use  number for compare
+		
+//		Compare | and ||.
+//		| -> don't have dead code and it use number for compare
+//		|| -> have dead code and it not use  number for compare
+//		Perform bitwise operations on user input.
+		
+		
+
+	}
+
+}
